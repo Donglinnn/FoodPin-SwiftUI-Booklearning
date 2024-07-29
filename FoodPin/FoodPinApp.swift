19e55680c85aa812001ba980221c26bec7a97cfb
Donglinnn <65762101+Donglinnn@users.noreply.github.com>
2024-07-29 10:40:27 +0800

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Che-lun Hu on 2024/7/29.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
